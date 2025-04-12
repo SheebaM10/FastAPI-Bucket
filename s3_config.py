ea3c8e4db001000f7c5482226a6064d4424ff5ae
Grifith Sheeba Menon <Sheebam@ad.karanji.com>
2025-04-12 15:25:19 +0530

    first commit

---
+++ s3_config.py
@@ -0,0 +1,17 @@
+import boto3
+import os
+from dotenv import load_dotenv
+ 
+load_dotenv()  # Load values from .env file
+ 
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_REGION = os.getenv("AWS_REGION")
+BUCKET_NAME = os.getenv("BUCKET_NAME")
+ 
+s3_client = boto3.client(
+    "s3",
+    region_name=AWS_REGION,
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+)
